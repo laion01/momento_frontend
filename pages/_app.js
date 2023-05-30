11cf6f9cb67335719b22a6c72defc3a4d4ed796a
c-spider <spdrdev@gmail.com>
2023-05-30 09:24:20 -0400

    hot fix

--- pages/_app.js
+++ pages/_app.js
@@ -5,13 +5,24 @@ import '../styles/globals.css';
 
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import { useRouter } from 'next/router';
+import Image from 'next/image';
 
 function MyApp({ Component, pageProps }) {
+  const router = useRouter();
 
   return (
     <Provider store={store}>
+        <div>
+            
+        </div>
         <Layout>
-          <Component {...pageProps} />
+          {router.isFallback ? 
+            <div className='fixed w-[100vw] h-[100vh] top-0 left-0 flex justify-center items-center'>
+              <Image width={64} height={64} src="/images/loading.svg" alt="" />
+            </div>: 
+            <Component {...pageProps} />
+          }
         </Layout>
         <ToastContainer />
     </Provider>
